27daf5a40e8b001f85fa3e0396b5fcd09488b9f5
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-10 19:19:52 +0500

    .

--- Exam_5/Adonet.cs
+++ Exam_5/Adonet.cs
@@ -1,7 +1,34 @@
-﻿namespace Exam_5
+﻿using System.Data.SqlClient;
+
+namespace Exam_5
 {
     public class Adonet
     {
+        public static void GettAll(string tableName, string DatabaseName, string shart)
+            {
+                using (SqlConnection conn = new SqlConnection($"Server = (localdb)\\MSSQLLocalDB;Database={DatabaseName}; Trusted_Connection = true;"))
+                {
+                    conn.Open();
+                    string query = $"select * from {tableName} where {shart};";
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    using (SqlDataReader rdr = cmd.ExecuteReader())
+                    {
+                        int count = rdr.FieldCount;
+                        while (rdr.Read())
+                        {
+                            for (int i = 0; i < count; i++)
+                            {
+                                Console.WriteLine($"Col{i} {rdr[i]}");
+                            }
+                            Console.WriteLine();
+                        }
+                    }
+                }
+
+            }
+        
+
 
     }
 }
