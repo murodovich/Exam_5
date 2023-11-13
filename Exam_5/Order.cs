f8c9e150be1d3531e11349606afc7a37953b2ba4
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-13 21:05:19 +0500

    sa

--- Exam_5/Order.cs
+++ Exam_5/Order.cs
@@ -1,14 +1,12 @@
-﻿namespace Exam_5
+﻿namespace Exam_5;
+public class Order
 {
-    public class Order
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-        public string Address { get; set; }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public string Address { get; set; }
 
 
 
 
-    }
 }
