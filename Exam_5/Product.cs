fcb72df012fe65aa01716366654489387cad200c
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-13 21:05:43 +0500

    asd

--- Exam_5/Product.cs
+++ Exam_5/Product.cs
@@ -1,9 +1,7 @@
-﻿namespace Exam_5
+﻿namespace Exam_5;
+public class Product
 {
-    public class Product
-    {
-        public  int Id { get; set; }
-        public string Name { get; set; }
-        public decimal Narxi { get; set; }
-    }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Narxi { get; set; }
 }
