c43490eb72a3fb18f298eb16c81c961cb832745f
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-13 21:04:58 +0500

    ass

--- Exam_5/Oquvchi.cs
+++ Exam_5/Oquvchi.cs
@@ -1,10 +1,9 @@
-﻿namespace Exam_5
+﻿namespace Exam_5;
+
+public class Oquvchi
 {
-    public class Oquvchi
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string DarsName { get; set; }
-        public int age { get; set; }    
-    }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string DarsName { get; set; }
+    public int age { get; set; }
 }
