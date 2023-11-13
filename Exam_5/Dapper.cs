c9e36bca1cc03a019775ff66f7f74f4303511b46
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-13 21:04:11 +0500

    save

--- Exam_5/Dapper.cs
+++ Exam_5/Dapper.cs
@@ -7,14 +7,14 @@ namespace Exam_5
     {
         public static void GettAll(string tableName, string DatabaseName, string shart)
         {
-            using(SqlConnection conn = new SqlConnection($"Server = (localdb)\\MSSQLLocalDB;Database={DatabaseName}; Trusted_Connection = true;"))
+            using (SqlConnection conn = new SqlConnection($"Server = (localdb)\\MSSQLLocalDB;Database={DatabaseName}; Trusted_Connection = true;"))
             {
                 conn.Open();
                 string query = $"select * from {tableName} where {shart};";
-                var obj = conn.Execute(query) ;
-                
+                var obj = conn.Execute(query);
+
                 conn.Close();
-                              
+
 
             }
         }
